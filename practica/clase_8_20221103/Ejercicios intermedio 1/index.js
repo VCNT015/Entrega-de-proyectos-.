38168943dd80a9dacc3aa1fd1bee1e591fafe1fa
VCNT015 <59272883+VCNT015@users.noreply.github.com>
2022-11-08 18:59:52 -0300

    Nueva funcionalidad

---
+++ practica/clase_8_20221103/Ejercicios intermedio 1/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+// este ejemplo es para cuando la carpeta esta dentro de otra
+//app.use(express.static('public/assets'));
+app.use(express.static('assets'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/vistas/index.html");
+});
+
+app.get('/formulario', function (req, res) {
+    res.sendFile(__dirname + "/vistas/formulario.html");
+});
+
+
+app.listen(80)
+console.log("Servidor web corriendo: http://localhost:80")
